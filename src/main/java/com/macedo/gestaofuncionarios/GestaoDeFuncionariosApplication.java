382932af58f502e0c5814cca458c1968525366a1
MaccedoFilho <ricardomacedor@icloud.com>
2025-05-12 00:06:40 -0300

    chore: Configura projeto Spring Boot com estrutura inicial e dependências

---
+++ src/main/java/com/macedo/gestaofuncionarios/GestaoDeFuncionariosApplication.java
@@ -0,0 +1,13 @@
+package com.macedo.gestaofuncionarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoDeFuncionariosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestaoDeFuncionariosApplication.class, args);
+    }
+
+}
