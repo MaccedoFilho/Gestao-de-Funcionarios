8da4c95806c9b8540b65852168023ea63f9d8331
MaccedoFilho <ricardomacedor@icloud.com>
2025-05-26 00:57:34 -0300

    Feat: Finalizando config do Spring Security

--- src/main/java/com/macedo/gestaofuncionarios/config/SecurityConfig.java
+++ src/main/java/com/macedo/gestaofuncionarios/config/SecurityConfig.java
@@ -1,4 +1,29 @@
 package com.macedo.gestaofuncionarios.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorizeRequests ->
+                        authorizeRequests
+                                .requestMatchers(
+                                        "/swagger-ui/**",
+                                        "/v3/api-docs/**",
+                                        "/api/**"
+                                ).permitAll()
+                                .anyRequest().authenticated()
+                );
+        return http.build();
+    }
 }
